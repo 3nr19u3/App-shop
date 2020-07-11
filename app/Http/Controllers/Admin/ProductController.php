176fc43e3e763b12b187ecbd787fd37fb1470f71
k1k3 <kike123kob@gmail.com>
2020-07-11 15:11:57 -0500

    App advances

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::paginate(10);
+        return view('admin.products.index')->with(compact('products'));//inyecta datos a la vista
+    }
+    
+    public function create()
+    {
+        return view('admin.products.create');
+    }
+
+    public function store(Request $request)
+    {
+        //Messages
+        $messages =[
+            'name.required' => 'Es necesario ingresar un nombre para el producto.',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres.',
+            'description.required' => 'Es necesario ingresar una descripcion para el producto.',
+            'description.max' => 'El la descripcion del producto debe tener como maximo 200 caracteres.',
+            'price.required' => 'Es necesario ingresar el precio del producto.',
+            'price.numeric' => 'El precio del producto debe ser numerico.',
+            'price.min' => 'Precio dle producto invalido.'
+
+        ];
+
+        //Validar
+        $rules =[
+            'name' => 'required|min:3',
+            'description' => 'required|max:200',
+            'price' => 'required|numeric|min:0'
+        ];
+
+        $this->validate($request,$rules,$messages);
+        //dd($request->all());
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->long_description = $request->input('long_description');
+        $product->save();//INSERT
+
+        return redirect('/admin/products');
+    }
+
+
+    public function edit($id)
+    {
+        //return "Mostrar aqui el form para el producto con id $id";
+        $product = Product::find($id);
+        return view('admin.products.edit')->with(compact('product'));//form de edicion
+    }
+
+    public function update(Request $request, $id)
+    {
+        //Messages
+        $messages =[
+            'name.required' => 'Es necesario ingresar un nombre para el producto.',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres.',
+            'description.required' => 'Es necesario ingresar una descripcion para el producto.',
+            'description.max' => 'El la descripcion del producto debe tener como maximo 200 caracteres.',
+            'price.required' => 'Es necesario ingresar el precio del producto.',
+            'price.numeric' => 'El precio del producto debe ser numerico.',
+            'price.min' => 'Precio dle producto invalido.'
+
+        ];
+
+        //Validar
+        $rules =[
+            'name' => 'required|min:3',
+            'description' => 'required|max:200',
+            'price' => 'required|numeric|min:0'
+        ];
+
+        $this->validate($request,$rules,$messages);
+        //dd($request->all());
+        $product = Product::find($id);
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->long_description = $request->input('long_description');
+        $product->save();//UPDATE
+
+        return redirect('/admin/products');
+    }
+
+    public function destroy($id)
+    {
+        //dd($request->all());
+        $product = Product::find($id);
+        $product->delete();//Elimina
+
+        return back();
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Product;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +14,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $products = Product::paginate(12);
+    return view('welcome')->with(compact('products'));
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/products/{id}', 'ProductController@show');
+
+Route::post('/cart', 'CartDetailController@store');
+Route::delete('/cart', 'CartDetailController@destroy');
+
+Route::post('/order', 'CartController@update');
+
+
+Route::middleware(['auth','admin'])->prefix('admin')->namespace('Admin')->group(function(){
+Route::get('/products', 'ProductController@index');//Listado
+Route::get('/products/create', 'ProductController@create');//formulario
+Route::post('/products', 'ProductController@store');//registrar
+Route::get('/products/{id}/edit', 'ProductController@edit');//formulario de edicion
+Route::post('/products/{id}/edit', 'ProductController@update');//Actualiza
+Route::post('/products/{id}/delete', 'ProductController@destroy');//formulario eliminar
+
+Route::get('/products/{id}/images', 'ImageController@index');//Lista imagenes de los productos
+Route::post('/products/{id}/images', 'ImageController@store');//Registra la imagen del producto
+Route::delete('/products/{id}/images', 'ImageController@destroy');//Elimina la imagen del producto
+Route::get('/products/{id}/images/select/{image}', 'ImageController@select');//Destacar una imagen
+});
+
+/**
+ * ADMIN:
+ * kike123kob@gmail.com
+ * k1k3k1k3$$$
+ * USSER:
+ * luis@hotmail.com
+ * luisluis123
+ */
